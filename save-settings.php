0bda1dc57b04a8bc7b4e52a851ea384d8d74fd94
Jovan Kitanovic <88426652+jovaann@users.noreply.github.com>
2023-11-07 14:54:32 +0100

    Update save-settings.php
    Fix: issue with meta boxes content disappearing after editing page via elementor

--- save-settings.php
+++ save-settings.php
@@ -1,14 +1,22 @@
-
 <?php
 // Save Code when the page is updated or published
 function save_script_code($post_id) {
+    // Check for autosave and user permissions
     if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
     if (!current_user_can('edit_post', $post_id)) return;
 
-    $script_code_head = isset($_POST['script_awesome_code_head']) ? $_POST['script_awesome_code_head'] : [];
-    $script_code_body = isset($_POST['script_awesome_code_body']) ? $_POST['script_awesome_code_body'] : [];
+    // Define the post meta keys
+    $meta_keys = array(
+        'script_awesome_code_head' => '_script_awesome_code_head',
+        'script_awesome_code_body' => '_script_awesome_code_body',
+    );
 
-    update_post_meta($post_id, '_script_awesome_code_head', $script_code_head);
-    update_post_meta($post_id, '_script_awesome_code_body', $script_code_body);
+    // Iterate through the meta keys and update their values
+    foreach ($meta_keys as $input_key => $meta_key) {
+        if (isset($_POST[$input_key])) {
+            $script_code = $_POST[$input_key];
+            update_post_meta($post_id, $meta_key, $script_code);
+        }
+    }
 }
 add_action('save_post', 'save_script_code');
